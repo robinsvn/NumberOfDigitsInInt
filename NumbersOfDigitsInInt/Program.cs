7167f77ae594b53aa898f1a7bcf16a19a77a77ff
robinsvn <111857815+robinsvn@users.noreply.github.com>
2022-11-17 10:01:19 +0100

    Add files via upload

---
+++ NumbersOfDigitsInInt/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumbersOfDigitsInInt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                int number = 0, count = 0;
+                Console.Write("Skriv ditt nummer :");
+                number = Convert.ToInt32(Console.ReadLine());
+                while (number > 0)
+                {
+                    number = number / 10;
+                    count++;
+                }
+                Console.WriteLine("Mengden av sifre er : " + count);
+                Console.ReadLine();
+
+        }
+    }
+}
